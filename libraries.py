9ad21b53bd9280ee44fc9dd4237fe7766acb1d48
Giammarco Bozzelli <giammarcobozzelli3006@gmail.com>
2023-12-21 23:23:57 +0100

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -1,25 +1,24 @@
-import importlib
+def libraries(self):
+        self.libraries = ['numpy', 'pandas', 'seaborn', 'matplotlib', 'plotly', 'scipy', 'scikit-learn', 'statsmodels', 'linearmodels', 'stargazer', 'pycountry']
 
-dlibraries = ['numpy', 'pandas', 'seaborn', 'matplotlib', 'plotly', 'scipy', 'scikit-learn', 'statsmodels', 'linearmodels', 'stargazer', 'pycountry']
+        self.missing_libraries = [lib for lib in self.libraries if importlib.util.find_spec(lib) is None]
+        if self.missing_libraries:
+            for lib in self.missing_libraries:
+                !pip install {lib}
+        else:
+            print("All required libraries are already installed.")
 
-missing_libraries = [lib for lib in dlibraries if importlib.util.find_spec(lib) is None]
-
-if missing_libraries:
-    for lib in missing_libraries:
-        !pip install {lib}
-else:
-    print("All required libraries are already installed.")
-
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.express as px
-import scipy.stats as stats
-from IPython.display import display
-from sklearn.linear_model import LinearRegression
-import statsmodels.api as sm
-from linearmodels.panel import PanelOLS
-from stargazer.stargazer import Stargazer
-import pycountry
-from IPython.display import Image, display,HTML
+def import_common_libraries():
+    import numpy as np
+    import pandas as pd
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    import plotly.express as px
+    import scipy.stats as stats
+    from IPython.display import display
+    from sklearn.linear_model import LinearRegression
+    import statsmodels.api as sm
+    from linearmodels.panel import PanelOLS
+    from stargazer.stargazer import Stargazer
+    import pycountry
+    from IPython.display import Image, display, HTML
