a62d30aece50e2052541552e74ee9698dbc5d557
Giammarco Bozzelli <giammarcobozzelli3006@gmail.com>
2023-12-22 12:14:57 +0100

    Update Modules.py

--- Modules.py
+++ Modules.py
@@ -16,6 +16,38 @@ def plot_1(df):
   plt.grid(True)
   plt.show()
 
+def graphe(): # plot graph to illustrate the Dif-in-dif theoretical model
+    x1 = np.linspace(0, 10, 100)
+    y1 = 1.5 * x1 + 4
+
+    x2 = np.linspace(0, 10, 100)
+    y2 = x2 + 4
+
+    x3 = np.linspace(0, 10, 100)
+    y3 = x3 + 2
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(x1, y1, color='orange', label='Treated group')
+    plt.plot(x2, y2, linestyle='--', color='orange', label='Hypothetical treated group without treatment')
+    plt.plot(x3, y3, color='green', label='Control group')
+
+    plt.text(x1[0], y1[0], 'c', fontsize=12, color='black', ha='right', va='bottom')
+    plt.text(x1[-1], y1[-1], 'd', fontsize=12, color='black', ha='right', va='bottom')
+
+    plt.text(x2[0], y2[0], '', fontsize=12, color='black', ha='right', va='bottom')
+    plt.text(x2[-1], y2[-1], "d'", fontsize=12, color='black', ha='right', va='bottom')
+
+    plt.text(x3[0], y3[0], 'a', fontsize=12, color='black', ha='right', va='bottom')
+    plt.text(x3[-1], y3[-1], 'b', fontsize=12, color='black', ha='right', va='bottom')
+
+    plt.title('DID theoretical model')
+    plt.xlabel('Time')
+    plt.ylabel('Outcome')
+    plt.legend()
+
+    return plt.show()
+
+
 def ratioger_bel():
     sample = ['germany', 'belgium']
 
