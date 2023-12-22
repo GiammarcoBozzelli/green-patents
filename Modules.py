2f0acb5275938c1e412b07e3141be5baa362db6a
Giammarco Bozzelli <giammarcobozzelli3006@gmail.com>
2023-12-22 12:38:25 +0100

    Update Modules.py

--- Modules.py
+++ Modules.py
@@ -74,3 +74,84 @@ def ratioger_bel():
     plt.grid(True)
     plt.show()
 
+def difdifger_bel():
+  plt.figure(figsize=(10, 6))
+
+#Sort data from 1990 to 2021
+  germany_data_filtered = germany_data[(germany_data['Year'] >= 1990) & (germany_data['Year'] <= 2021)]
+  predictions_germany_filtered = predictions_germany[(germany_data['Year'] >= 1990) & (germany_data['Year'] <= 2021)]
+
+  belgium_data_filtered = belgium_data[(belgium_data['Year'] >= 1990) & (belgium_data['Year'] <= 2021)]
+  predictions_belgium_filtered = predictions_belgium[(belgium_data['Year'] >= 1990) & (belgium_data['Year'] <= 2021)]
+
+#regression for germany
+  plt.scatter(germany_data_filtered['Year'], germany_data_filtered['Patent per Capita'], label='Patent per Capita - Germany', color='red')
+  plt.plot(germany_data_filtered['Year'], predictions_germany_filtered, label='Linear Regression - Germany', color='salmon')
+
+#regression for austria
+  plt.scatter(belgium_data_filtered['Year'], belgium_data_filtered['Patent per Capita'], label='Patent per Capita - Belgium', color='blue')
+  plt.plot(belgium_data_filtered['Year'], predictions_belgium_filtered, label='Linear Regression - Belgium', color='lightblue')
+
+#shift line
+  plt.axvline(x=2002, color='green', linestyle='--', label='Boundary Before/After')
+
+  plt.xlabel('Year')
+  plt.ylabel('Patent per Capita')
+  plt.title('Linear Regression for Germany and Belgium between 1990 and 2021 with Before/After Boundary')
+  plt.legend()
+  plt.grid(True)
+  plt.show()
+
+
+def ratiotha_mal():
+  sample = ['thailand', 'malaysia']
+
+  plt.figure(figsize=(15, 6))
+  for pays in sample:
+    data_selected = cata[(cata['Country'] == pays) & (cata['Year'].between(1995, 2022))]
+    catastrophes_per_year = data_selected['Year'].value_counts().sort_index()
+    damage_per_year = data_selected.groupby('Year')["Total Affected"].sum().fillna(0)
+
+    ratio = damage_per_year / catastrophes_per_year
+
+    if pays == 'malaysia':
+            plt.plot(ratio.index, ratio.values, marker='o', linestyle='-', label=pays, color='purple')
+    elif pays == 'thailand':
+            plt.plot(ratio.index, ratio.values, marker='o', linestyle='-', label=pays, color='orange')
+
+  #plt.plot(ratio.index, ratio.values, marker='o', linestyle='-', label=pays)
+
+  plt.xlabel('Year')
+  plt.ylabel('Total Damage per Catastrophe')
+  plt.title('Ratio of Total Damage per Catastrophe for Selected Countries between 1980 and 2021')
+  plt.legend()
+  plt.grid(True)
+  plt.show()
+
+def difdiftha_mal():
+  plt.figure(figsize=(10, 6))
+
+#Sort data from 1990 to 2021
+  thailand_data_filtered = thailand_data[(thailand_data['Year'] >= 1995) & (thailand_data['Year'] <= 2021)]
+  predictions_thailand_filtered = predictions_thailand[(thailand_data['Year'] >= 1995) & (thailand_data['Year'] <= 2021)]
+
+  malaysia_data_filtered = malaysia_data[(malaysia_data['Year'] >= 1995) & (malaysia_data['Year'] <= 2021)]
+  predictions_malaysia_filtered = predictions_malaysia[(malaysia_data['Year'] >= 1995) & (malaysia_data['Year'] <= 2021)]
+
+#regression for Brazil
+  plt.scatter(thailand_data_filtered['Year'], thailand_data_filtered['Patent per Capita'], label='Patent per Capita - Thailand', color='orange')
+  plt.plot(thailand_data_filtered['Year'], predictions_thailand_filtered, label='Linear Regression - Thailand', color='orange')
+
+#regression for germany
+  plt.scatter(malaysia_data_filtered['Year'], malaysia_data_filtered['Patent per Capita'], label='Patent per Capita - Malaysia', color='purple')
+  plt.plot(malaysia_data_filtered['Year'], predictions_malaysia_filtered, label='Linear Regression - Malaysia', color='purple')
+
+#shift line
+  plt.axvline(x=2010, color='green', linestyle='--', label='Boundary Before/After')
+
+  plt.xlabel('Year')
+  plt.ylabel('Patent per Capita')
+  plt.title('Linear Regression for Thailand and Malaysia between 1995 and 2021 with Before/After Boundary')
+  plt.legend()
+  plt.grid(True)
+  plt.show()
